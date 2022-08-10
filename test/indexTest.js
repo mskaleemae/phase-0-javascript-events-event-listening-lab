6cbbde1e0ae4560404e231e06a72a8748ee9552e
Kaeli Gilmore <mskaleemae2000@gmail.com>
2022-08-10 13:33:09 -0400

    domeeventsredo

--- test/indexTest.js
+++ test/indexTest.js
@@ -11,5 +11,12 @@ describe("index.js", () => {
   it("binds an event listener in addingEventListener()", () => {
     addingEventListener();
     expect(input.addEventListener.called).to.be.true;
+    const input = document.getElementById('button');
+
+function clickAlert() {
+  alert('I was clicked!');
+}
+
+input.addEventListener('click', clickAlert);
   })
 })
